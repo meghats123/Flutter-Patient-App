c605a40919a3f54625db812e3fc1856fd75d396d
meghats123 <meghats39032@gmail.com>
2020-11-19 21:51:40 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:patient_app/screens/patient.dart';
 
 void main()=>runApp(MyApp());
 class MyApp extends StatelessWidget{
-  TextEditingController getPatientid=TextEditingController();
-TextEditingController getPatientname=TextEditingController();
-TextEditingController getSymptoms=TextEditingController();
-TextEditingController getAge=TextEditingController();
-TextEditingController getMedicine=TextEditingController();
-TextEditingController getDoctorname=TextEditingController();
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,81 +16,7 @@ TextEditingController getDoctorname=TextEditingController();
           title: Text("Patient App"),
           backgroundColor: Colors.amber,
         ),
-        body: Container(
-          padding: EdgeInsets.all(10.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextField(
-                  controller: getPatientid,
-                  decoration: InputDecoration(
-                    hintText: "Patient id",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  controller: getPatientname,
-                  decoration: InputDecoration(
-                    hintText: "Patient Name",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  controller: getSymptoms,
-                  decoration: InputDecoration(
-                    hintText: "Symptoms",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  controller: getAge,
-                  decoration: InputDecoration(
-                    hintText: "Age",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  controller: getMedicine,
-                  decoration: InputDecoration(
-                    hintText: "Medicine",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                TextField(
-                  controller: getDoctorname,
-                  decoration: InputDecoration(
-                    hintText: "Doctor Name",
-                    border: OutlineInputBorder()
-                  ),
-                ),
-                SizedBox(height: 10.0,),
-                RaisedButton(onPressed: (){
-                  String getPid=getPatientid.text;
-                  String getPname=getPatientname.text;
-                  String getSymp=getSymptoms.text;
-                  String getAg=getAge.text;
-                  String getMed=getMedicine.text;
-                  String getDname=getDoctorname.text;
-                  print(getPid);
-                  print(getPname);
-                  print(getSymp);
-                  print(getAg);
-                  print(getMed);
-                  print(getDname);
-
-                },
-                  color: Colors.amber,
-                  child: Text("SUBMIT"),
-                )
-              ],
-            ),
-          ),
-        ),
+        body: CheckApp(),
       ),
     );
   }
---
+++ lib/screens/patient.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+
+class CheckApp extends StatefulWidget {
+  @override
+  _CheckAppState createState() => _CheckAppState();
+}
+
+class _CheckAppState extends State<CheckApp> {
+  TextEditingController getPatientid=TextEditingController();
+  TextEditingController getPatientname=TextEditingController();
+  TextEditingController getSymptoms=TextEditingController();
+  TextEditingController getAge=TextEditingController();
+  TextEditingController getMedicine=TextEditingController();
+  TextEditingController getDoctorname=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: getPatientid,
+              decoration: InputDecoration(
+                  hintText: "Patient id",
+                  border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextField(
+              controller: getPatientname,
+              decoration: InputDecoration(
+                  hintText: "Patient Name",
+                  border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextField(
+              controller: getSymptoms,
+              decoration: InputDecoration(
+                  hintText: "Symptoms",
+                  border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextField(
+              controller: getAge,
+              decoration: InputDecoration(
+                  hintText: "Age",
+                  border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextField(
+              controller: getMedicine,
+              decoration: InputDecoration(
+                  hintText: "Medicine",
+                  border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            TextField(
+              controller: getDoctorname,
+              decoration: InputDecoration(
+                  hintText: "Doctor Name",
+                  border: OutlineInputBorder()
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            RaisedButton(onPressed: (){
+              String getPid=getPatientid.text;
+              String getPname=getPatientname.text;
+              String getSymp=getSymptoms.text;
+              String getAg=getAge.text;
+              String getMed=getMedicine.text;
+              String getDname=getDoctorname.text;
+              print(getPid);
+              print(getPname);
+              print(getSymp);
+              print(getAg);
+              print(getMed);
+              print(getDname);
+
+            },
+              color: Colors.amber,
+              child: Text("SUBMIT"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
